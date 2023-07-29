f32030628cf735a824a5ad485d4f12c2e7e6eec8
DDonghyeo <gsu9045000@gmail.com>
2023-07-30 00:13:15 +0900

    Feat : 초기 설정

---
+++ src/main/java/com/pippo/ppiyong/PpiyongApplication.java
@@ -0,0 +1,13 @@
+package com.pippo.ppiyong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PpiyongApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PpiyongApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pippo/ppiyong/PpiyongApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pippo.ppiyong;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PpiyongApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
