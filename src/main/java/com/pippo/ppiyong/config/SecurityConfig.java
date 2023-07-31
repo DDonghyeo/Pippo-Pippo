d10841697bea7b9bffb67df9d60402be3e5442eb
DDonghyeo <gsu9045000@gmail.com>
2023-07-31 13:05:05 +0900

    Feat : Security Config

---
+++ src/main/java/com/pippo/ppiyong/config/SecurityConfig.java
@@ -0,0 +1,73 @@
+package com.pippo.ppiyong.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    private final CustomUserDetailsService userDetailsService;
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+
+                // 회원가입, 로그인 등 인증이 필요없는 요청에 대해서는 전체허용 (permitAll)
+                // 그 외 모든 요청 (any) 에 대해서는 인증 요구
+                .authorizeHttpRequests((authorizeRequest) ->
+                        authorizeRequest
+                                //아래 링크는 모든 통신 허용
+                                .requestMatchers("/login").permitAll()
+                                //외에 다른 Request는 인증되어야 함
+                                .anyRequest().authenticated()
+                )
+
+                // Rest 방식으로 로그인을 할 것이므로 form 로그인 사용 안함
+                .formLogin(AbstractHttpConfigurer::disable)
+
+                // logout url 설정. 인증을 위해 사용하는 쿠키인 JSESSIONID를 삭제하여 로그아웃 처리
+                .logout((configurer) ->
+                        configurer
+                                .logoutUrl("/api/users/logout")
+                                .deleteCookies("JSESSIONID")
+                )
+
+                //인증되지 않은 자원에 접근했을 때
+                .exceptionHandling((configurer) ->
+                        configurer
+                                .authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.FORBIDDEN)));
+
+
+        return http.build();
+    }
+
+
+    //인증 작업을 해줄
+    @Bean
+    public AuthenticationManager authenticationManager() {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(userDetailsService);
+        authProvider.setPasswordEncoder(passwordEncoder());
+        return new ProviderManager(authProvider);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
