8c881c71797d378bd6a7af58c0db2c5fcb5eb5ae
DDonghyeo <gsu9045000@gmail.com>
2023-08-08 21:58:43 +0900

    [Feat] CORS Error

--- src/main/java/com/pippo/ppiyong/config/SecurityConfig.java
+++ src/main/java/com/pippo/ppiyong/config/SecurityConfig.java
@@ -17,7 +17,9 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.HttpStatusEntryPoint;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebSecurity
@@ -72,6 +74,16 @@ public class SecurityConfig {
 
 
         return http.build();
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("*");
+            }
+        };
     }
 
 
