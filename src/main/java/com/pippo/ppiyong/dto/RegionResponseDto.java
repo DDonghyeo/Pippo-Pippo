5f645cdaba8016e8d3251482ba03800fdb509fc0
noeyoes <lsy021128@naver.com>
2023-08-11 01:14:20 +0900

    Feat: 알림 지역 조회 구현

--- src/main/java/com/pippo/ppiyong/dto/RegionResponseDto.java
+++ src/main/java/com/pippo/ppiyong/dto/RegionResponseDto.java
@@ -2,7 +2,9 @@ package com.pippo.ppiyong.dto;
 
 import com.pippo.ppiyong.domain.User;
 import com.pippo.ppiyong.type.Region;
+import lombok.Getter;
 
+@Getter
 public class RegionResponseDto {
     private Region region;
 
