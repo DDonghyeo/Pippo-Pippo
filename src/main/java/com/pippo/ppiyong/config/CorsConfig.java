2863413cb47de5852edd96c5a4f1dcd0045279fe
DDonghyeo <gsu9045000@gmail.com>
2023-08-12 22:46:48 +0900

    Fix : Cors

--- src/main/java/com/pippo/ppiyong/config/CorsConfig.java
+++ src/main/java/com/pippo/ppiyong/config/CorsConfig.java
@@ -12,8 +12,8 @@ public class CorsConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/api/**") // API 엔드포인트에 대해서만 적용할 수도 있습니다.
-                .allowedOrigins("*") // 허용할 오리진(도메인)을 지정합니다.
+        registry.addMapping("/**") // 모든 엔드포인트에 대해 CORS 설정을 적용합니다.
+                .allowedOrigins("*") // 모든 오리진을 허용합니다.
                 .allowedMethods("GET", "POST", "PUT", "DELETE") // 허용할 HTTP 메서드를 지정합니다.
                 .allowCredentials(true) // 쿠키를 포함할 경우 true로 설정합니다.
                 .exposedHeaders("Set-Cookie"); // 노출할 응답 헤더를 지정합니다.
