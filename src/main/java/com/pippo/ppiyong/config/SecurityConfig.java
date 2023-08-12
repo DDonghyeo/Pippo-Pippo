12ef1da786f2f3ca38a892cf0a757d68e36ec1d8
DDonghyeo <gsu9045000@gmail.com>
2023-08-12 23:16:10 +0900

    Fix : Cors

--- src/main/java/com/pippo/ppiyong/config/SecurityConfig.java
+++ src/main/java/com/pippo/ppiyong/config/SecurityConfig.java
@@ -100,7 +100,11 @@ public class SecurityConfig {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**").allowedOrigins("*");
+                registry.addMapping("/**") // 모든 엔드포인트에 대해 CORS 설정을 적용합니다.
+                        .allowedOrigins("http://localhost:8000","http://localhost:81","http://localhost:5500","http://localhost:8080") // 모든 오리진을 허용합니다.
+                        .allowedMethods("GET", "POST", "PUT", "DELETE") // 허용할 HTTP 메서드를 지정합니다.
+                        .allowCredentials(true) // 쿠키를 포함할 경우 true로 설정합니다.
+                        .exposedHeaders("Set-Cookie"); // 노출할 응답 헤더를 지정합니다.
             }
         };
     }
