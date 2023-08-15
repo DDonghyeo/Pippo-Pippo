6a132b7e742ba414185adcf03ecf23827ab56bbd
DDonghyeo <gsu9045000@gmail.com>
2023-08-15 15:25:15 +0900

    Fix : Security Config

--- src/main/java/com/pippo/ppiyong/config/SecurityConfig.java
+++ src/main/java/com/pippo/ppiyong/config/SecurityConfig.java
@@ -101,6 +101,7 @@ public class SecurityConfig {
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
+        configuration.addAllowedOrigin("https://page.ppiyong.shop");
         configuration.setAllowedOrigins(Arrays.asList("http://localhost:8000","http://localhost:81","http://localhost:5500","http://localhost:8080", "http://127.0.0.1:81/", "https://page.ppiyong.shop/"));
         configuration.setAllowedMethods(Arrays.asList("GET","POST", "OPTIONS", "PUT","DELETE"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
