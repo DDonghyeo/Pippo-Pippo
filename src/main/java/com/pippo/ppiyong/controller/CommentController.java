a714d1782d8f5b0e4dd97eed98ec50b03343063b
Jiyun82 <202110918@sangmyung.kr>
2023-08-18 15:43:59 +0900

    Fix: transactional 추가

--- src/main/java/com/pippo/ppiyong/controller/CommentController.java
+++ src/main/java/com/pippo/ppiyong/controller/CommentController.java
@@ -13,11 +13,13 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.Optional;
 
+@Transactional
 @RestController
 @RequestMapping("/api")
 public class CommentController {
